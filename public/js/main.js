35b6811618d264bb86a3e7a8153aa0c222415a87
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-11 09:34:01 -0600

    added code for login and submit features

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,8 @@ if (fb.getAuth()) {
   $('.loggedIn').toggleClass('hidden');
 }
 
+// Login
+
 $('.login input[type="button"]').click(function(event){
   var $loginForm = $(event.target).closest('form'),
       email = $loginForm.find('[type="email"]').val(),
@@ -56,32 +58,56 @@ $('.logout').click(function(){
   location.reload(true);
 })
 
-$tbody.on('click', 'td', function (evt) {
+// Friends
+
+$tbody.on('click', '.delete', function (evt) {
   // this = event.target;
   var $tr  = $(evt.target).closest('tr'),
       uuid = $tr.data('uuid'),
-      friendName = $tr.find('td').text();
+      deleteUser = $tr.find('.delete').text();
 
-  if (confirmFriendRemoval(friendName)) {
+  if (confirmFriendRemoval(deleteUser)) {
     $tr.remove();
     deleteFriendFromDb(uuid);
   }
 });
 
 $form.submit(function (evt) {
-  var $friendName = $('input[name="friendName"]'),
-      req         = {name: $friendName.val()};
+  var $firstName = $('input[name="firstName"]'),
+      $lastName  = $('input[name="lastName"]'),
+      $phone  = $('input[name="phoneNumber"]'),
+      $email  = $('input[name="email"]'),
+      $address  = $('input[name="address"]'),
+      $twitter  = $('input[name="twitter"]'),
+      $github  = $('input[name="github"]'),
+      req         = {firstName: $firstName.val(),
+                     lastName: $lastName.val(),
+                     phone: $phone.val(),
+                     email: $email.val(),
+                     address: $address.val(),
+                     twitter: $twitter.val(),
+                     github: $github.val(),
+      };
 
   evt.preventDefault();
 
   addFriendToDb(req, function (res) {
-    var $tr = $('<tr><td>' + req.name + '</td></tr>');
+    console.log(req)
+    console.log(res)
+    var gravatar = $('<img>').attr({src: 'http://www.gravatar.com/avatar/' + md5(req.email)});
+    var $tr = $('<tr><td><img src="'+gravatar[0].currentSrc+'"></img></td><td>' + req.firstName + '</td><td>' +req.lastName+ '</td><td>' + req.phone + '</td><td><a href="mailto:'+req.email+'">' + req.email + '</a></td><td>' + req.address + '</td><td><a href="https://twitter.com/'+req.twitter+'"target="_blank">' + req.twitter + '</a></td><td><a href="https://github.com/' + req.github+'" target="_blank">' +req.github + '</a></td><td class="delete">Delete '+req.firstName+' '+req.lastName+'</td>></tr>' );
 
-    $tr.attr('data-uuid', res.name);
-    $tbody.append($tr);
+    $tr.attr('data-uuid', res.uuid);
+  $tbody.append($tr);
   });
 
-  $friendName.val('');
+  $firstName.val('');
+  $lastName.val('');
+  $phone.val('');
+  $email.val('');
+  $address.val('');
+  $twitter.val('');
+  $github.val('');
 });
 
 function addFriendToDb(data, cb) {
@@ -98,7 +124,8 @@ function deleteFriendFromDb(uuid) {
 }
 
 function addRowToTable(uuid, data) {
-  var $tr = $('<tr><td>' + data.name + '</td></tr>');
+  var gravatar = $('<img>').attr({src: 'http://www.gravatar.com/avatar/' + md5(data.email)});
+  var $tr = $('<tr><td><img src="'+gravatar[0].currentSrc+'"></img></td><td>' + data.firstName + '</td><td>' +data.lastName+ '</td><td>' + data.phone + '</td><td><a href="mailto:'+data.email+'">' + data.email + '</a></td><td>' + data.address + '</td><td><a href="https://twitter.com/'+data.twitter+'"target="_blank">' + data.twitter + '</a></td><td><a href="https://github.com/' + data.github+'" target="_blank">' + data.github + '</a></td><td class="delete">Delete '+data.firstName+' '+data.lastName+'</td>></tr>' );
 
   $tr.attr('data-uuid', uuid);
   $tbody.append($tr);
